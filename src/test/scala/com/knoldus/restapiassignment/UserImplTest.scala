1379aa58fe1f628c057582d07888c7288cfa437b
Gaurav <gaurav.raj@knoldus.com>
2022-03-01 01:30:10 +0530

    "test cases and functionality"

--- src/test/scala/com/knoldus/restapiassignment/UserImplTest.scala
+++ src/test/scala/com/knoldus/restapiassignment/UserImplTest.scala
@@ -12,9 +12,10 @@ class UserImplTest extends AnyFlatSpec {
   val userRepo = new UserImpl
   val gaurav = User(1,"Gaurav", 25, "gauravraj.raj49@gmail.com")
   val bhavya = User(id=2,name = "bhavya", age = 25 ,email = "bhavya@knoldus.com")
+  userRepo.addUser("Gaurav", 25, "gauravraj.raj49@gmail.com")
+  userRepo.addUser(name = "rishabh", age = 26, emailId="reshu@cloudcover.com")
 
   it should "addUser" in {
-    userRepo.addUser("Gaurav", 25, "gauravraj.raj49@gmail.com")
     assertResult(ListBuffer(gaurav))(userRepo.getUser("Gaurav"))
   }
 
@@ -25,7 +26,6 @@ class UserImplTest extends AnyFlatSpec {
 
   it should "deleteUser" in {
     val rishabh = User(id=2,name = "rishabh", age = 26, email="reshu@cloudcover.com")
-    userRepo.addUser(name = "rishabh", age = 26, emailId="reshu@cloudcover.com")
     assertResult(ListBuffer(gaurav))(userRepo.deleteUser(rishabh.id))
   }
 
